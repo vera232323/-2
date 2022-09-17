29b35b476b0c0fd2cfb9217ccceccac8bcd67c1a
Vera <imbesom@mail.ru>
2022-09-17 20:25:58 +0300

    ДЗ2

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Пиши первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пиши второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пиши третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    if(a>c)
+{
+    Console.WriteLine(a + " "+ "- наибольшее число");
+}
+else
+{
+    Console.WriteLine(c + " "+ "- наибольшее число");
+}
+}
+else
+{
+    if(b>c)
+    {
+      Console.WriteLine(b + " "+ " - наибольшее число");  
+    }
+    else
+    {
+        Console.WriteLine(c + " "+ " - наибольшее число"); 
+    }
+}
